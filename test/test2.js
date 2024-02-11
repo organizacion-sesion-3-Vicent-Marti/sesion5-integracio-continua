7fecff164b3f4f2365bd12dc9242cb9e0c862954
Vicent Martí <vicentp.marti@gmail.com>
2024-02-11 17:12:30 +0100

    Actualizar un pequeño cambio en test2

--- test/test2.js
+++ test/test2.js
@@ -11,7 +11,9 @@ it('comprobar función resta', function() {
   operaciones.resta.should.be.a.Function();
   should.equal(operaciones.resta(7,4), 3);
   should.equal(operaciones.resta(1,3), -2);
-	should.equal(operaciones.resta(1,3), -2);
+  should.equal(operaciones.resta(1,3), -2);
+  // añadimos esta línea para ver como afecta en el repositorio remoto
+  should.equal(operaciones.resta(3,3), 0);
 });
 
 
